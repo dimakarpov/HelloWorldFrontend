f7507c28cef579c0af08841b90a91511bad99e5e
Dima Karpov <dima.karpov@justeattakeaway.com>
2022-03-06 23:49:55 +0200

    Add form + submit functionality (CORS issue not resolved)

--- my-app/src/EventSimulator/index.js
+++ my-app/src/EventSimulator/index.js
@@ -3,25 +3,60 @@ import ReactDOM from 'react-dom';
 import './EventSimulator.css';
 
 export default class EventSimulator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            last_leading_date: '',
+            pre_trial_date: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: event.target.value
+        });
+      }
+    
+      handleSubmit(event) {
+        alert('SUBMIT! Last pleading: ' + this.state.last_leading_date + ' | Pre trial: ' + this.state.pre_trial_date);
+
+        fetch('https://localhost:7296/EventSimulator?lastPleadingDate=' + this.state.last_leading_date + '&preTrialDate=' + this.state.pre_trial_date)
+        .then(response => response.json())
+        .then(data => console.log(data));
+        // .then(response => { 
+        //     //response.json();
+        //     alert("data: " + response.json());
+        // })
+        // .then(data => { 
+        //     console.log(data);
+        //     alert("data: " + data);
+        // });
+
+        event.preventDefault();
+    }
+
     render() {
       return (
         <div>
-          <div>
+          <form  onSubmit={this.handleSubmit}>
                 <label>Last pleading date:
-                    <input type="date" name="last-pleading-date" placeholder='dd-mm-yyyy' />
+                    <input type="date" name="last_leading_date" value={this.state.value} onChange={this.handleChange} placeholder='dd-mm-yyyy' />
                 </label>
-            <br />
+                <br />
                 <label>Pre trial date:
-                    <input type="date" name="pre-trial-date" placeholder='dd-mm-yyyy' />
+                    <input type="date" name="pre_trial_date" value={this.state.value} onChange={this.handleChange} placeholder='dd-mm-yyyy' />
                 </label>
-            <br />
-            <button onClick={GetEventSimulation}>Run Simulation</button>
-          </div>
+                <br />
+            <input type="submit" value="Run Simulation" />
+          </form>
         </div>
       );
     }
-  }
-
-  function GetEventSimulation() {
-    alert("!");
   }
